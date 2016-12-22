147bdbe73a3dca21e75a495b0aa4cf4685a1b878
bladerangel <bladerangel10@gmail.com>
2016-12-22 01:46:08 -0300

    final version

--- src/main/java/controllers/CompositionOperationsController.java
+++ src/main/java/controllers/CompositionOperationsController.java
@@ -34,6 +34,8 @@ public class CompositionOperationsController extends AbstractController implemen
     private Collection<Event> eventsAccessible1;
     private Collection<Event> eventsAccessible2;
 
+    private Collection<Event> notConnect;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -45,6 +47,7 @@ public class CompositionOperationsController extends AbstractController implemen
         super.init(applicationController, layoutGraph1, layoutGraph2);
         statesAccessible1 = new ArrayList<>();
         statesAccessible2 = new ArrayList<>();
+        notConnect = new ArrayList<>();
 
         eventsAccessible1 = new ArrayList<>();
         eventsAccessible2 = new ArrayList<>();
@@ -58,19 +61,19 @@ public class CompositionOperationsController extends AbstractController implemen
         pane1.getChildren().add(swingNode);
         foundSuccessors1(cloneLayoutGraph1.getStateStart());
         foundSuccessors2(cloneLayoutGraph2.getStateStart());
+        multiplication();
 
     }
 
     @FXML
-    public void product() {
-        statesAccessible1.forEach(state1 -> {
-            statesAccessible2.forEach(state2 -> {
-                cloneLayoutGraph.addState(new State(state1.getName() + "," + state2.getName()));
-            });
-        });
+    public void multiplication() {
+        statesAccessible1.forEach(state1 ->
+                statesAccessible2.forEach(state2 ->
+                        cloneLayoutGraph.addState(new State(state1.getName() + "," + state2.getName()))));
 
         State stateAnterior1 = cloneLayoutGraph1.getStateStart();
         State stateAnterior2 = cloneLayoutGraph2.getStateStart();
+
         for (Event event1 : eventsAccessible1) {
             for (Event event2 : eventsAccessible2) {
                 if (event1.getLinkName().equals(event2.getLinkName()) && event1.getStateInit().equals(stateAnterior1) && event2.getStateInit().equals(stateAnterior2)) {
@@ -95,11 +98,9 @@ public class CompositionOperationsController extends AbstractController implemen
 
     @FXML
     public void parallel() {
-        statesAccessible1.forEach(state1 -> {
-            statesAccessible2.forEach(state2 -> {
-                cloneLayoutGraph.addState(new State(state1.getName() + "," + state2.getName()));
-            });
-        });
+        statesAccessible1.forEach(state1 ->
+                statesAccessible2.forEach(state2 ->
+                        cloneLayoutGraph.addState(new State(state1.getName() + "," + state2.getName()))));
 
         State stateAnterior1 = cloneLayoutGraph1.getStateStart();
         State stateAnterior2 = cloneLayoutGraph2.getStateStart();
@@ -113,46 +114,21 @@ public class CompositionOperationsController extends AbstractController implemen
                     cloneLayoutGraph.addEvent(event, state1, state2);
                     stateAnterior1 = event1.getStateFinal();
                     stateAnterior2 = event2.getStateFinal();
-
                 }
             }
         }
 
-        createAndSetSwingContent();
-    }
+        setNotConnect();
 
-    @FXML
-    public void original() {
         createAndSetSwingContent();
+
     }
 
     public void createAndSetSwingContent() {
-
         swingNode.setContent(cloneLayoutGraph.changeBasicVisualizationServer());
         cloneLayoutGraph = new LayoutGraph();
-
+        notConnect = new ArrayList<>();
     }
-
-    public boolean contains(State state) {
-
-        for (State state1 : cloneLayoutGraph.getAllStates()) {
-            if (state1.getName().equals(state.getName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean containsEvent(Event event) {
-
-        for (Event event1 : cloneLayoutGraph.getAllEvents()) {
-            if (event1.getLinkName().equals(event.getLinkName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 
     public void foundSuccessors1(State state) {
         if (!statesAccessible1.contains(state) && cloneLayoutGraph1.getStatesSuccessors(state) != null) {
@@ -175,6 +151,43 @@ public class CompositionOperationsController extends AbstractController implemen
                         eventsAccessible2.add(cloneLayoutGraph2.findEvent(state, stateSuccessors));
                         foundSuccessors2(stateSuccessors);
                     });
+        }
+    }
+
+    public void setNotConnect() {
+        notConnect.addAll(eventsAccessible1);
+        for (Event event1 : eventsAccessible1) {
+            for (Event event2 : eventsAccessible2) {
+                if (event1.getLinkName().equals(event2.getLinkName())) {
+                    notConnect.remove(event1);
+                }
+            }
+        }
+        notConnect.forEach(event -> {
+            for (State event2 : statesAccessible2) {
+                State state1 = cloneLayoutGraph.findStateByName(event.getStateInit().getName() + "," + event2.getName());
+                State state2 = cloneLayoutGraph.findStateByName(event.getStateFinal().getName() + "," + event2.getName());
+                cloneLayoutGraph.addEvent(new Event(event.getLinkName(), state1, state2), state1, state2);
+            }
+
+        });
+        if (notConnect.isEmpty()) {
+            notConnect.addAll(eventsAccessible2);
+            for (Event event2 : eventsAccessible2) {
+                for (Event event1 : eventsAccessible1) {
+                    if (event1.getLinkName().equals(event2.getLinkName())) {
+                        notConnect.remove(event2);
+                    }
+                }
+            }
+            notConnect.forEach(event -> {
+                for (State event2 : statesAccessible1) {
+                    State state1 = cloneLayoutGraph.findStateByName(event2.getName() + "," + event.getStateInit().getName());
+                    State state2 = cloneLayoutGraph.findStateByName(event2.getName() + "," + event.getStateFinal().getName());
+                    cloneLayoutGraph.addEvent(new Event(event.getLinkName(), state1, state2), state1, state2);
+                }
+
+            });
         }
     }
 
